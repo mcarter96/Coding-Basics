be5dd73efd29945373db557ac6e4b278240463a6
Lucas Carter <luke05@gmail.com>
2019-07-15 18:09:29 -0700

    Im done with assisgnment one

---
+++ HelloWorld/Assignment1.py
@@ -0,0 +1,16 @@
+# Print "Hello world" to the screen
+# Print "Hello [your name" to the screen
+# Print your name and your age to the screen
+# Print the substring "my first" from the string "This is my first message"
+
+print("Hello World")
+print()
+print("Hello Lucas")
+print()
+
+Year=2019
+x= Year - 2005-1
+print("Lucas Carter", x)
+print()
+array= "This is my first message"
+print (array[8:16])
