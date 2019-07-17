019c6736802e76c54cf8e12996f513717c0e83cd
Matt Carter <m6carter@comcast.net>
2019-07-17 13:35:35 -0700

    Added second assignment

---
+++ inputFromKeyboard/assignment2.py
@@ -0,0 +1,2 @@
+# Write a program that asks the user for their name and respond "Hello, [their name]"
+# Come up with three different questions to ask the user and print them out in a list under their name
