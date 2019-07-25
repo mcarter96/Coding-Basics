b737ba0c36f475e585d79bf50ef1601bf9140b61
Matt Carter <m6carter@comcast.net>
2019-07-25 14:35:04 -0700

    Added assignment 3

---
+++ ifAndsOrButs/assignment3.py
@@ -0,0 +1,5 @@
+# Part 1
+# Write a program that takes user input on what score they got on an assignment. Then use if-else statements to tell the user
+# what grade they got on their assignment.
+# Part 2
+# Take three numbers from the user and then print them out in sequential order (from least to greatest)
