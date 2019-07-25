41263f1b65784bdd774dbbab6c87e30b21234760
Matt Carter <m6carter@comcast.net>
2019-07-25 16:45:47 -0700

    added assignment4

---
+++ functions/assignment4.py
@@ -0,0 +1,12 @@
+# Part 1
+# Use functions to get the sum from a series of numbers that the user inputs
+# Part 2
+# Use functions to get the max number from the series of numbers already inputed by the user
+# 
+# Example Output:
+# Enter the first number: 4
+# Enter the second number: 1
+# Enter the third number: 5
+# Enter the fourth number: 6
+# The sum of your numbers is: 16
+# The max number is: 6
