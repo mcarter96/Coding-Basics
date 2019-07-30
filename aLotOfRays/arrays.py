8bdff248387d35b331289b15a105b0366a43e83a
Matt Carter <m6carter@comcast.net>
2019-07-30 17:04:11 -0600

    started on arrays lesson

---
+++ aLotOfRays/arrays.py
@@ -0,0 +1,20 @@
+# Arrays are super useful in programming because they allow you to store multiple values
+# in one variable.
+cars = ["Jeep", "Audi", "BMW"]
+# Having an array of cars in easier than having:
+car1 = "Jeep"
+car2 = "Audi"
+car3 = "BMW"
+
+# To Access the elements of an array, you use the index number. The start of tha array is 
+# index 0 and goes on from there. So you can access the first element of the cars array by 
+# doing this:
+
+print("Example 1")
+firstCar = cars[0]
+print(firstCar)
+
+print(cars[1])
+
+# You can also use the index to modify what is stored at that index in the array. For example,
+# if you wanted to change the se
