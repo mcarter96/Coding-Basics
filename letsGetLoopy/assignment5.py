83e3a8b0bfe9f3fe294486a4ac8b7e306d9ead21
Matt Carter <m6carter@comcast.net>
2019-07-30 16:50:36 -0600

    Added loops notes and assignment5

---
+++ letsGetLoopy/assignment5.py
@@ -0,0 +1,19 @@
+# Part 1:
+# Print all the odd numbers in a range given by the user
+# 
+# Example output:
+# Enter the first number: 5
+# Enter the second number: 15
+# 5 7 9 11 13
+
+# Part 2:
+# Print the following pattern using nested for loops
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
