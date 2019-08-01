cd60fbf88fb5a8bff376487bcc97a312270e5f29
Matt Carter <m6carter@comcast.net>
2019-08-01 11:31:10 -0600

    Added randomNumber notes and assignment7

---
+++ randomNumbers/assignment7.py
@@ -0,0 +1,8 @@
+# Part 1
+# Generate a series of random numbers between 1 and 100 and put them in an array, then sort the array. Print both of them
+# Example Output:
+# Unsorted Array - [18, 97, 43, 1, 37, 88, 77, 57, 79, 48]
+# Sorted Array - [1, 37, 43]
+array = [18, 97, 43, 1, 37, 88, 77, 57, 79, 48]
+array.sort()
+print(array)
